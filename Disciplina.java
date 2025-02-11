7fec0ac61f287bda1c17025b1ec3382f061edcdd
CarlosEduardoTorquatoRodrigues <145603092+CarlosEduardoTorquatoRodrigues@users.noreply.github.com>
2025-02-11 01:05:30 +0000

    list

--- Disciplina.java
+++ Disciplina.java
@@ -9,10 +9,41 @@ public class Disciplina {
         this("", new ArrayList<Aluno>(), new ArrayList<Professor>());
     }
     public Disciplina(String nomeDisciplina){
-
+        this(nomeDisciplina,new ArrayList<Aluno>(), new ArrayList<Professor>());
     }
 
     public Disciplina(String nomeDisciplina, List<Aluno> alunos, List<Professor> professores){
+        this.nomeDisciplina = nomeDisciplina;
+        this.alunos = alunos;
+        this.professores = professores;
+    }
 
+    public void setnomeDisciplina(string nomeDisciplina){
+        this.nomeDisciplina = nomeDisciplina;
+    }
+    
+    public int getNomeDisplina(){
+        return this.nomeDisciplina;
+    }
+
+    public void setAlunos(List<Aluno> alunos){
+        this.alunos = alunos;
+    }
+    
+    public List<Aluno> getAlunos(){
+        return this.alunos;
+    }
+
+    public void setProfessores(List<Professor> professores){
+        this.professores = professores;
+    }
+    
+    public List<Professor> getProfessores(){
+        return this.professores;
+    }
+
+    @Override
+    public String toString(){
+        return "Disciplina{" + super.toString() + ", disciplina = " + getNomeDisplina() + "}";
     }
 }
