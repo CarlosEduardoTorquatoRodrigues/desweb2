d53ffff387e617c1b53f776e3464381e1055802a
CarlosEduardoTorquatoRodrigues <145603092+CarlosEduardoTorquatoRodrigues@users.noreply.github.com>
2025-02-11 00:21:34 +0000

    10/02/2025

--- Principal.java
+++ Principal.java
@@ -2,13 +2,7 @@ public class Principal{
 
     public static void main(String args[]){
 
-        Pessoa objPessoa = new Pessoa();
-        System.out.println(objPessoa.toString());
-
-        Pessoa objPessoa2 = new Pessoa("Rafael", 34);
-        System.out.println(objPessoa2.toString());
-
-        Pessoa objPessoa3 = new Pessoa("Gabriel", 27, 10000000001L);
-        System.out.println(objPessoa3.toString());
+        Aluno aluno1 = new Aluno("Rafael", 34,123436466437l,1001);
+        System.out.println(aluno1.toString());
     }
 }
