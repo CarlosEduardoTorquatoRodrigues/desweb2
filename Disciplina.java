5bbb57a34b9c1bd6b13569dd867f60e392ae4b9f
CarlosEduardoTorquatoRodrigues <145603092+CarlosEduardoTorquatoRodrigues@users.noreply.github.com>
2025-02-12 22:00:24 +0000

    lista disciplina concluida

--- Disciplina.java
+++ Disciplina.java
@@ -1,56 +1,59 @@
 import java.util.List;
+import java.util.ArrayList;
 
 public class Disciplina {
     private String nomeDisciplina;
     private List<Aluno> alunos;
     private List<Professor> professores;
 
-    public Disciplina(){
+    public Disciplina() {
         this("", new ArrayList<Aluno>(), new ArrayList<Professor>());
     }
-    public Disciplina(String nomeDisciplina){
-        this(nomeDisciplina,new ArrayList<Aluno>(), new ArrayList<Professor>());
+
+    public Disciplina(String nomeDisciplina) {
+        this(nomeDisciplina, new ArrayList<Aluno>(), new ArrayList<Professor>());
     }
 
-    public Disciplina(String nomeDisciplina, List<Aluno> alunos, List<Professor> professores){
+    public Disciplina(String nomeDisciplina, List<Aluno> alunos, List<Professor> professores) {
         this.nomeDisciplina = nomeDisciplina;
         this.alunos = alunos;
         this.professores = professores;
     }
 
-    public void setnomeDisciplina(String nomeDisciplina){
+    public void setNomeDisciplina(String nomeDisciplina) {
         this.nomeDisciplina = nomeDisciplina;
     }
-    
-    public String getNomeDisplina(){
+
+    public String getNomeDisciplina() {
         return this.nomeDisciplina;
     }
 
-    public void setAlunos(List<Aluno> alunos){
+    public void setAlunos(List<Aluno> alunos) {
         this.alunos = alunos;
     }
-    
-    public List<Aluno> getAlunos(){
+
+    public List<Aluno> getAlunos() {
         return this.alunos;
     }
 
-    public void setProfessores(List<Professor> professores){
+    public void setProfessores(List<Professor> professores) {
         this.professores = professores;
     }
-    
-    public List<Professor> getProfessores(){
+
+    public List<Professor> getProfessores() {
         return this.professores;
     }
 
-    public void addAluno(Aluno aluno){
-        this.aluno.add(aluno);
+    public void addAluno(Aluno aluno) {
+        this.alunos.add(aluno);
     }
-    public void addProfessor(Professor professor){
-        this.professor.add(professor);
+
+    public void addProfessor(Professor professor) {
+        this.professores.add(professor);
     }
 
     @Override
-    public String toString(){
-        return "Disciplina{" + super.toString() + ", disciplina = " + getNomeDisplina() + ", alunos = " + getAlunos() +", professores = " + getProfessores() +"}";
+    public String toString() {
+        return "Disciplina{" + super.toString() + ", disciplina = " + getNomeDisciplina() + ", alunos = " + getAlunos() + ", professores = " + getProfessores() + "}";
     }
 }
--- Principal.java
+++ Principal.java
@@ -7,5 +7,16 @@ public class Principal{
 
         Professor professor1 = new Professor("Rafael", 34,123436466437l,10);
         System.out.println(professor1.toString());
+
+        Disciplina  disciplina = new  Disciplina("Web 2");
+        
+
+        disciplina.addAluno(aluno1);
+        disciplina.addProfessor(professor1);
+        System.out.println(disciplina.toString());
+
+        
+        
+
     }
 }
