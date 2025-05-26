03adc7a09a9c9830c8192578329477f2e99f7a0f
CarlosEduardoTorquatoRodrigues <145603092+CarlosEduardoTorquatoRodrigues@users.noreply.github.com>
2025-05-26 15:56:53 +0000

    ok

--- LucasDomingues_pratica3/pratica3/src/main/java/com/example/pratica3/repository/VeiculoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.pratica3.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.example.pratica3.model.Veiculo;
-
-@Repository
-public interface VeiculoRepository extends JpaRepository<Veiculo, Long> {
-}
